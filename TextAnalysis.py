7e525bc27bc7c77e3da2fb9d07d0cfd7bfd8bd55
mystified131 <mystifiedthomas@gmail.com>
2018-11-24 21:48:50 -0600

    touch-ups

--- TextAnalysis.py
+++ TextAnalysis.py
@@ -1,45 +1,38 @@
 def text_processor(superman):
-  setfin = []
-  super = []
-  wonder = {}
-  super = superman.split()
-  for x in range(len(super)):
-    wonder[super[x]] = 1
+    inpi2 = superman.lower()
+    inpi3 = ""
+    ctr = 0
+    for jar in inpi2:
+        if inpi2[ctr].isalpha() or inpi2[ctr].isnumeric() or inpi2[ctr] == " ":
+            inpi3 = inpi3 + jar
+        ctr = ctr + 1
+    setfin = []
+    super = []
+    wonder = {}
+    super = inpi3.split()
+    for x in range(len(super)):
+        wonder[super[x]] = 1
  
-  for elm in range(len(super)):
-    if super[elm] not in setfin:
-      setfin.append(super[elm])
-    else:  
-      wonder[super[elm]] += 1
+    for elm in range(len(super)):
+        if super[elm] not in setfin:
+            setfin.append(super[elm])
+        else:  
+            wonder[super[elm]] += 1
 
-  print("")
-  print("how many:")
-  replist2 = []
-  totstr = ""
-  for elm3 in wonder:
-    repstr = str(wonder[elm3]/100) + ": " + elm3
-    replist2.append(repstr)
-  replist2.sort(reverse = True)
-  for e in replist2:
-    print(e)
-    valstr = e[:4]
-    vala = float(valstr)
-    valb = vala * 100
-    val = int(valb)
-    for ex in range(val):
-        astr = e[6:]
-        totstr += astr + " "
-  print("")
-  print("reconstruct by word frequency:")    
-  print(totstr)
+    replist2 = []
+    totstr = ""
+    for elm3 in wonder:
+        repstr = str(wonder[elm3]/100) + ": " + elm3
+        replist2.append(repstr)
+    replist2.sort(reverse = True)
+    for e in replist2:
+        valstr = e[:4]
+        vala = float(valstr)
+        valb = vala * 100
+        val = int(valb)
+        for ex in range(val):
+            astr = e[6:]
+            totstr += astr + " "
+    return totstr
 
-inpi = input("Please enter a phrase made of letters or numbers: ")
-inpi2 = inpi.lower()
-inpi3 = ""
-ctr = 0
-for jar in inpi2:
-  if inpi2[ctr].isalpha() or inpi2[ctr].isnumeric() or inpi2[ctr] == " ":
-    inpi3 = inpi3 + jar
-  ctr = ctr + 1
-
-text_processor(inpi3)
+  ## THE GHOST OF THE SHADOW ##
